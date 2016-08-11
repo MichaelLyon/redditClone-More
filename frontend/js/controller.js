d628be5b3b253f5d1e2dfca53918525a4e3a3cdf
Michael Tobin <mikel.tobin@gmail.com>
2016-08-11 17:16:15 -0600

    Working submit form. Utilizing bootstrap forms and ng-submit

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -3,19 +3,26 @@ var app = angular.module('redditClone.controllers', [])
 app.controller('mainController', ['$scope', '$http', function($scope, $http) {
   var redditSubs = this
   $scope.subredditObj = {};
-
-  $scope.subSelected = '';
-
+  $scope.sumbitPostShowFormBool = false;
 
   $scope.getPosts = function(subName){
     $http.get(`https://www.reddit.com/r/${subName}/top/.json?limit=50`).then(function(data) {
       $scope.subredditObj.posts = data.data.data.children;
+      console.log($scope.subredditObj.posts);
   	})
   }
     getSubs = function () {
     $http.get('https://www.reddit.com/reddits.json?limit=50&after=t3_10omtd/').then(function(data) {
       $scope.subredditObj.subs = data.data.data.children
+
   	})
+  }
+  $scope.showSubmitForm = function () {
+    $scope.sumbitPostShowFormBool = true;
+  }
+  $scope.submit =function (post) {
+    $scope.sumbitPostShowFormBool = false;
+    console.log(post);
   }
 
 getSubs();

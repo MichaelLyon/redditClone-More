ab6eb52f8d0e292adddb03ecedefe9fac2c63e31
Michael Tobin <mikel.tobin@gmail.com>
2016-08-11 11:26:43 -0600

    Hooked up bootstrap and got the basic layout for the reddit clone set up. Need to assign ajax call data to scope vars and ng-repeat them

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -1,9 +1,12 @@
 var app = angular.module('redditClone.controllers', [])
 
 app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+  $scope.subreddit = {};
+  $scope.subreddit.title;
+  $scope.subreddit.imageURL;
+  $scope.subreddit.brief;
 	$http.get('https://www.reddit.com/r/webdev/top/.json?limit=50').then(function(data) {
 		console.log(data.data.data);
 	})
-	console.log('aayyyo sayn its cha boi ');
 
 }])
